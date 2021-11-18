e7c40fb4a813949a659109f32b03da02454f95fb
Foxtrotts <74994722+Foxtrotts@users.noreply.github.com>
2021-11-17 19:17:31 -0500

    Added all 'Fraser Couighs' + Javascript

--- soundboard.js
+++ soundboard.js
@@ -1,5 +1,15 @@
 
+function cough1() {
+    var audio= new Audio('sounds/cough1.wav');
+    audio.play();
+}
+
+function cough2() {
+    var audio= new Audio('sounds/cough2.wav');
+    audio.play();
+}
+
 function cough3() {
-    var audio= new Audio('sounds/ahem_x.wav');
+    var audio= new Audio('sounds/cough3.wav');
     audio.play();
 }
